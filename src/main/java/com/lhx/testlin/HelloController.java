eefad798d34fb5ee51b7732d1b7232eca0b6b27d
lhx <1742577641@qq.com>
2021-05-11 17:42:01 +0800

    111222222111

--- src/main/java/com/lhx/testlin/HelloController.java
+++ src/main/java/com/lhx/testlin/HelloController.java
@@ -13,7 +13,6 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(){
         User name = helloMapper.getName();
-
         System.out.println(111222111);
         return name.toString();
 
