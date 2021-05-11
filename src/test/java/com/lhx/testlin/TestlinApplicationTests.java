9d663952bd68436d51a5a6cb6f6addee2351275e
lhx <1742577641@qq.com>
2021-05-11 15:34:46 +0800

    创建了service

---
+++ src/test/java/com/lhx/testlin/TestlinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhx.testlin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestlinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
